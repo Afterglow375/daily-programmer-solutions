6145aed46e0095ff4cadaac9b5e34f0c39024559
Alex Tatusko <alex.tatusko@gmail.com>
2014-07-14 16:46:20 -0400

    Worked on Daily169

--- java/Daily169/src/Daily169.java
+++ java/Daily169/src/Daily169.java
@@ -1,4 +1,4 @@
-// Challenge #158 [Hard] Intersecting Rectangles
+// Challenge #169 [Hard] Convex Polygon Area
 // http://bit.ly/1qRrBRO
 
 import java.io.File;
@@ -11,34 +11,77 @@ public class Daily169 {
 		File input = new File("src/input.txt");
 		
 		try {
+			// Read and parse input
 			Scanner sc = new Scanner(input);
 			int numCoords = sc.nextInt();
 			String[] xyArr;
-			ArrayList<Double[]> coordArr = new ArrayList<Double[]>(numCoords);
+			ArrayList<Point> coordArr = new ArrayList<Point>(numCoords);
 			
 			for (int i = 0; i < numCoords; i++) {
 				xyArr = sc.next().split(",");
-				Double[] xyArrInts = {Double.parseDouble(xyArr[0]), Double.parseDouble(xyArr[1])};
-				coordArr.add(xyArrInts);
+				Point point = new Point(Double.parseDouble(xyArr[0]), Double.parseDouble(xyArr[1]));
+				coordArr.add(point);
 			}
 			
-			for (int i = 0; i < numCoords-2; i++) {
-				coordArr.get(0);
+			// Divides up the polygon with non-overlapping triangles that all share an anchor point
+			// Find the two adjacent points of the anchor point
+			Point anchor = coordArr.remove(0); // Arbitrarily chosen
+			Point adjacentPoint1 = coordArr.remove(0);
+			Point adjacentPoint2;
+			int adjacentPoint2Index = 0;
+			double maxAngleSoFar = 0;
+			double angle = 0;
+			
+			
+			System.out.println(anchor.x + " " + anchor.y);
+			System.out.println(adjacentPoint1.x + " " + adjacentPoint1.y);
+			for (int i = 0; i < coordArr.size(); i++) {
+				angle = anchor.calcAngle(adjacentPoint1, coordArr.get(i));
+				System.out.println(coordArr.get(i).x + " " + coordArr.get(i).y + " angle: " + angle);
+				if (angle > maxAngleSoFar) {
+//					System.out.println(coordArr.get(i).x + " " + coordArr.get(i).y);
+					maxAngleSoFar = angle;
+					adjacentPoint2Index = i;
+				}
 			}
+			adjacentPoint2 = coordArr.remove(adjacentPoint2Index);
+			System.out.println(adjacentPoint2.x + " " + adjacentPoint2.y);
 			
-//			Double[] vert1 = {-10, -10};
-//			Double[] vert2 = {28, 15};
-//			Double[] vert3 = {18, 7};
-//			System.out.println(calcTriArea(vert1, vert2, vert3));
+			maxAngleSoFar = 0;
+			int adjacentPoint1Index = 0;
+			for (int i = 0; i < coordArr.size(); i++) {
+				angle = anchor.calcAngle(adjacentPoint2, coordArr.get(i));
+				if (angle > maxAngleSoFar) {
+					maxAngleSoFar = angle;
+					adjacentPoint1Index = i;
+				}
+			}
+			adjacentPoint1 = coordArr.remove(adjacentPoint1Index);
 			
+//			System.out.println(adjacentPoint1.x + " " + adjacentPoint1.y);
+//			System.out.println(adjacentPoint2.x + " " + adjacentPoint2.y);
+//			System.out.println(anchor.x + " " + anchor.y);
+//			
+//			double area = 0;
+//			while (coordArr.size() > 0) {
+//				Point testingPoint = coordArr.remove(0);
+//				for (int i = 0; i < coordArr.size(); i++) {
+//					if (anchor.)
+//				}
+//				coordArr.remove(0);
+//			}
+//			
+//			for (int i = 0; i < coordArr.size(); i++) {
+//				for (int j = 0; j < coordArr.size(); j++) {
+//					
+//				}
+//			}
+
+//			System.out.println(area);
 			sc.close();
 		}
 		catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
-	}
-	
-	public static double calcTriArea(Double[] vert1, Double[] vert2, Double[] vert3) {
-		return Math.abs((vert1[0]*(vert2[1] - vert3[1]) + vert2[0]*(vert3[1] - vert1[1]) + vert3[0]*(vert1[1] - vert2[1]))/2);
 	}
 }
---
+++ java/Daily169/src/Point.java
@@ -0,0 +1,41 @@
+
+public class Point {
+	public double x;
+	public double y;
+	public double limit;
+	
+	public static void main(String[] args) {
+		Point p1 = new Point(0, 0);
+		Point p2 = new Point(1, 0);
+		Point p3 = new Point(0, 1); 
+		System.out.println(p1.calcDistance(p2));
+		System.out.println(p1.calcDistance(p3));
+		System.out.println(p1.calcAngle(p2, p3));
+	}
+	
+	public Point(double x, double y) {
+		this.x = x;
+		this.y = y;
+		this.limit = 2; // A point can only be used at most in two unique triangles
+	}
+	
+	// Calculates the distance between this and a given point
+	public double calcDistance(Point point) {
+		return Math.sqrt((point.y - this.y)*(point.y - this.y) + (point.x - this.x)*(point.x - this.x));
+	}
+	
+	// Calculates the angle made by two vectors given two points using law of cosines
+	public double calcAngle(Point p1, Point p2) {
+		double dist1 = this.calcDistance(p1);
+		double dist2 = this.calcDistance(p2);
+		double dist3 = p1.calcDistance(p2);
+		System.out.println(dist1 + " " + dist2 + " " + dist3);
+		System.out.println((dist1*dist1 + dist2+dist2 - dist3*dist3) / (2 * dist1 * dist2));
+		return Math.toDegrees(Math.acos((dist1*dist1 + dist2+dist2 - dist3*dist3) / (2 * dist1 * dist2)));
+	}
+	
+	// Takes two points and calculates the created triangle's area
+	public double calcTriangleArea(Point p1, Point p2) {
+		return Math.abs((this.x*(p1.y - p2.y) + p1.x*(p2.y - this.y) + p2.x*(this.y - p1.y))/2);
+	}
+}
